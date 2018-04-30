3acf89d45b0f478314ea8c8d3bf3ddbf32c71d63
Poitier Stringer <pstringe@e1z3r9p21.42.us.org>
2018-04-30 13:00:02 -0700

    normed ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,11 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 17:05:08 by pstringe          #+#    #+#             */
-/*   Updated: 2018/04/27 19:24:10 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/04/30 12:59:36 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-/*
-**	this is the main function that initializes the arguments, copies the format string
-**	to a buffer until it encounters a '%'. Upon encountering a '%' will get the 
-**	modifiers and format flags. It will then pass this information to the convert
-**	function which will call the appropriate function to edit the buffer, Once the
-**	all conversions have been carried out, or there is no more space in the buffer,
-**	the buffer is printed to standard output and destroyed thereafter.
-*/
 
 int		ft_printf(const char *format, ...)
 {
