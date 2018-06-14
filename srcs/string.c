5ee880081f9f81fb82d22c39de82a7e89bf62d16
Poitier Stringer <pstringe@e1z3r5p20.42.us.org>
2018-06-13 19:57:32 -0700

    38/197

--- srcs/string.c
+++ srcs/string.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 08:16:09 by pstringe          #+#    #+#             */
-/*   Updated: 2018/06/13 16:28:33 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/06/13 19:51:32 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int		str(t_m *m, char buf[MAX])
 {
 	char	*arg;
+	int		l;
 	int		i;
 	int		p;
 	int		w;
@@ -24,6 +25,11 @@ int		str(t_m *m, char buf[MAX])
 	if (m->place->type == 12)
 		arg[0] = va_arg(m->ap, int);
 	arg = arg ? arg : "(null)";
+	if (!ft_strncmp(arg, "", ((l = ft_strlen(arg)) ? l : 1)))
+	{
+		arg = m->place->precision ? ft_strnew(m->place->precision) : arg;
+		m->place->precision = 0;
+	}
 	p = !m->place->precision ? ft_strlen(arg) : m->place->precision;
 	w = m->place->width - p;
 	i = -1;
