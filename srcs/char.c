f37a9c0aa89b6374695ed46958a4aa610c5be722
Poitier Stringer <pstringe@e1z3r3p10.42.us.org>
2018-06-19 17:53:08 -0700

    35 / 197

--- srcs/char.c
+++ srcs/char.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 15:33:58 by pstringe          #+#    #+#             */
-/*   Updated: 2018/06/19 16:42:47 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/06/19 17:52:03 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,13 @@ int		chr(t_m *m, char buf[MAX])
 	int		arg;
 	int 	w;
 	int		i;
-	int		p;
 
-	m->place->precision = m->place->precision == -1 ? 0 : m->place->precision;
-	p = !m->place->precision ? ((arg = va_arg(m->ap, int)) ? 1 : 0) : m->place->precision - 1;
-	w =	m->place->width - p;
+	w =	m->place->width - ((arg = va_arg(m->ap, int)) ? 1 : 0);
 	i = -1;
 	if (!(m->place->flags & MINUS))
 		while (w-- > 0)
 			buf[m->pos_b++] = ' ';
-	buf[m->pos_b++] = arg;
-	while (++i < p)
-		buf[m->pos_b++] = ' ';
+	buf[m->pos_b++] = !arg ? '\0' : arg;
 	if (m->place->flags & MINUS)
 		while (w-- > 0)
 			buf[m->pos_b++]	= ' ';
