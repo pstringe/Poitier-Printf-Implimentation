df0c9cefcdf0ca5d4971e8f3cc788cd22812f1a6
Poitier Stringer <pstringe@e1z3r7p9.42.us.org>
2018-07-22 15:27:34 -0700

    fixed p_break_1

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 09:40:33 by pstringe          #+#    #+#             */
-/*   Updated: 2018/07/22 14:01:37 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/07/22 15:23:11 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,19 @@ void	test_172()
 	ft_putchar('\n');
 }
 
+void	p_break_1()
+{
+	int a;
+	int b;
+
+	ft_putstr("p_break_1\n");
+	a = printf("A res: % .d\n", -1);
+	b = ft_printf("B res: % .d\n", -1);
+	printf("A ret: %d\n", a);
+	printf("B ret: %d\n", b);
+	ft_putchar('\n');
+}
+
 void	test_precision()
 {
 	test_93();
@@ -85,6 +98,7 @@ void	test_precision()
 	test_95();
 	test_167();
 	test_172();
+	p_break_1();
 }
 
 int 	main(void)
