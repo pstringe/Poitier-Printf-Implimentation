c539c0d2f1dadebd510c9381300e6043601aabdc
Poitier Stringer <pstringe@e1z3r5p18.42.us.org>
2018-07-23 13:34:24 -0700

    normed headerw

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 19:31:04 by pstringe          #+#    #+#             */
-/*   Updated: 2018/07/23 12:59:56 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/07/23 13:33:43 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,24 +45,24 @@ typedef struct	s_f
 	int		h;
 }				t_f;
 
-typedef struct	s_w
+typedef	struct	s_w
 {
-	struct	s_num *n;
-	char 	*w;
-	int		z;
-	int 	l;
-	int		e_char;
+	struct s_num	*n;
+	char			*w;
+	int				z;
+	int				l;
+	int				e_char;
 }				t_w;
 
-typedef struct 	s_num
+typedef struct	s_num
 {
 	int		base;
 	int		sign;
-	int 	idx;
+	int		idx;
 	char	b_conv[100];
-	int 	neg;
+	int		neg;
 	int		arg;
-	int 	p_len;
+	int		p_len;
 	int		w_len;
 }				t_num;
 
@@ -139,19 +139,19 @@ int				not(t_m *m, char buf[MAX]);
 */
 
 void			get_num(t_m *m, t_num *n);
-void 			unsigned_conversion(t_m *m, t_num *n);
-void 			signed_conversion(t_m *m, t_num *n);
-void 			get_base(t_m *m, t_num *n);
+void			unsigned_conversion(t_m *m, t_num *n);
+void			signed_conversion(t_m *m, t_num *n);
+void			get_base(t_m *m, t_num *n);
 int				get_echar(t_num *n, t_w *spec, int flags);
 int				get_signchar(t_num *n, int flags);
 /*
-**	functions for applying modifications to num strings	
+**	functions for applying modifications to num strings
 */
 
 int				get_echar(t_num *n, t_w *spec, int flags);
-int 			get_signchar(t_num *n, int flags);
+int				get_signchar(t_num *n, int flags);
 void			num_prcs(t_num *n, int pr);
-void 			num_wdth(t_m *m, t_num *n, int wd, int flags);
+void			num_wdth(t_m *m, t_num *n, int wd, int flags);
 void			append(t_num *n, t_w *spec, int wd, int flags);
 void			prepend(t_num *n, t_w *spec, int wd, int flags);
 void			flags(t_m *m, t_num *n);
@@ -167,8 +167,7 @@ void			ft_upn(uintmax_t nb, t_num *n, t_m *m);
 **	misc functions for checking, output, cleanup ect ...
 */
 
-
-void 			skip_whitespace(t_m *m);
+void			skip_whitespace(t_m *m);
 int				replace(t_m *m, char buf[MAX], char *conv);
 int				is_flag(char c);
 void			put(t_m *m, char buf[MAX]);
