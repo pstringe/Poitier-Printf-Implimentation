0aee0910493e370fb6d7a387bbde1af5c6c2fd0b
Poitier Stringer <pstringe@e1z3r5p18.42.us.org>
2018-07-23 10:53:59 -0700

    normed digit.c

--- srcs/digit.c
+++ srcs/digit.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 06:49:20 by pstringe          #+#    #+#             */
-/*   Updated: 2018/07/20 19:42:45 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/07/23 10:53:01 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,17 @@
 
 int		dig(t_m *m, char buf[MAX])
 {
-	int 		ret;
-	char		*tmp;
-	t_num 		n;
+	int		ret;
+	char	*tmp;
+	t_num	n;
 
 	get_base(m, &n);
 	m->place->flags |= (m->place->type == 2 ? HASH : 0);
 	get_num(m, &n);
-	if (m->place->precision == 0 && !ft_strncmp(n.b_conv, "0", 1) && !(n.base == 8 && (m->place->flags & HASH)))
-	{	
+	if (m->place->precision == 0 &&
+			!ft_strncmp(n.b_conv, "0", 1) &&
+			!(n.base == 8 && (m->place->flags & HASH)))
+	{
 		tmp = ft_strnew(m->place->width);
 		ft_memset(tmp, ' ', m->place->width);
 		ret = replace(m, buf, tmp);
@@ -36,5 +38,5 @@ int		dig(t_m *m, char buf[MAX])
 	num_prcs(&n, m->place->precision);
 	num_wdth(m, &n, m->place->width, m->place->flags);
 	flags(m, &n);
-	return(replace(m, buf, n.b_conv));
+	return (replace(m, buf, n.b_conv));
 }
