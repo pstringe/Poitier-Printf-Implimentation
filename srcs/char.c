413ca215801f093d7b3ab7111912779ca8ef06fd
Poitier Stringer <pstringe@e1z3r7p9.42.us.org>
2018-07-22 19:37:51 -0700

    normed char.c:w

--- srcs/char.c
+++ srcs/char.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 15:33:58 by pstringe          #+#    #+#             */
-/*   Updated: 2018/06/19 18:19:07 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/07/22 19:36:43 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int		chr(t_m *m, char buf[MAX])
 {
-	int		arg;
-	int 	w;
-	int		i;
-	
+	int arg;
+	int w;
+	int i;
+
 	arg = va_arg(m->ap, int);
-	w =	m->place->width - 1;
+	w = m->place->width - 1;
 	i = -1;
 	if (!(m->place->flags & MINUS))
 		while (w-- > 0)
@@ -27,6 +27,6 @@ int		chr(t_m *m, char buf[MAX])
 	buf[m->pos_b++] = !arg ? '\0' : arg;
 	if (m->place->flags & MINUS)
 		while (w-- > 0)
-			buf[m->pos_b++]	= ' ';
+			buf[m->pos_b++] = ' ';
 	return (0);
-}	
+}
